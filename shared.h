aa38b84ebfb7b6d52ec89c7173b4a661099c1c66
Eric <ehuang48@my.bcit.ca>
2020-11-03 21:01:22 -0800

    Change port number to port 80

--- shared.h
+++ shared.h
@@ -6,6 +6,6 @@
 
 // https://www.sciencedirect.com/topics/computer-science/registered-port#:~:text=Ports%200%20through%201023%20are,be%20used%20dynamically%20by%20applications.
 // /etc/services
-#define PORT 49157
+#define PORT 80
 
 #endif
