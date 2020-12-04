a0061320ef686366d224feea0bcd01ceea15e2c4
Eric <ehuang48@my.bcit.ca>
2020-12-03 21:19:41 -0800

    Testing clear()

--- ncurses/ncurses_menu.c
+++ ncurses/ncurses_menu.c
@@ -112,6 +112,8 @@ void process_menu_input(MENU *menu, config_t *lib_config, WINDOW *window) {
             case 10: {   // ENTER KEY
                 ITEM *current = current_item(menu);
                 init_item_form(menu, current, lib_config);
+                clear();
+                refresh();
                 display_main_menu(menu, window);
                 break;
             }
